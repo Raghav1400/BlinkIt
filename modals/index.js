fe866738145a0660f12a5db8830722b1452baa6d
Raghav1400 <19ucs022@lnmiit.ac.in>
2023-02-16 09:53:56 +0530

    implement MVC

--- modals/index.js
+++ modals/index.js
@@ -1,1 +1,112 @@
-var second_header =["Vegetable & Fruits","Dairy & Breakfast","Munchies","Cold Drinks & Juices","Instant & Frozen Food","Tea, Coffee & Health Drinks","akery & Biscuits"]
+var second_header_content =["Vegetable & Fruits","Dairy & Breakfast","Munchies","Cold Drinks & Juices","Instant & Frozen Food","Tea, Coffee & Health Drinks","Bakery & Biscuits"];
+
+var aside_content=[
+    {
+        img_src:"./assects/fernando-andrade-nAOZCYcLND8-unsplash.jpg",
+        img_alt:"Fruits",
+        decription:"Fresh Fruits"
+    },
+    {
+        img_src:"./assects/fernando-andrade-nAOZCYcLND8-unsplash.jpg",
+        img_alt:"Fruits",
+        decription:"Fresh Fruits"
+    },
+    {
+        img_src:"./assects/fernando-andrade-nAOZCYcLND8-unsplash.jpg",
+        img_alt:"Fruits",
+        decription:"Fresh Fruits"
+    },
+    {
+        img_src:"./assects/fernando-andrade-nAOZCYcLND8-unsplash.jpg",
+        img_alt:"Fruits",
+        decription:"Fresh Fruits"
+    }
+]
+
+var section_bottom_content=[
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    {   
+        discount:"1% OFF",
+        img_src:"./assects/391305a.webp",
+        img_alt:"Potato Image",
+        source_time:"Source at 5AM",
+        name:"New Potato(Aloo)",
+        weight:"1Kg",
+        newPrice:"₹15",
+        oldPrice:"₹17" 
+    },
+    
+
+]
+
+
+export {second_header_content, aside_content,section_bottom_content};
